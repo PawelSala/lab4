dccb4d586f0cd31ee3ee7ec8afba52a610ed4855
PawelSala <pawel.sala01@wp.pl>
2024-04-06 15:39:52 +0200

    wyświetl datę

---
+++ skrypt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+case $1 in
+    --date|-d)
+        echo "Today's date: $(date)"
+        ;;
+    *)
+        echo "Error, need at least one argument"
+        exit 1
+        ;;
+esac
